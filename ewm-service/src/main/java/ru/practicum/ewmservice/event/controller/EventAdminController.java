772dce1591bf5fe08d274b24ba72a7146d896aae
Gauhar <gauharbegimova@gmail.com>
2023-06-19 11:59:29 +0600

    fix: back validate

--- ewm-service/src/main/java/ru/practicum/ewmservice/event/controller/EventAdminController.java
+++ ewm-service/src/main/java/ru/practicum/ewmservice/event/controller/EventAdminController.java
@@ -4,6 +4,7 @@ package ru.practicum.ewmservice.event.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin/events")
+@Validated
 public class EventAdminController {
     private final EventService eventService;
 
@@ -40,13 +42,13 @@ public class EventAdminController {
             @RequestParam(required = false) @DateTimeFormat(pattern = SystemConstats.DT_FORMAT) LocalDateTime rangeEnd,
             @RequestParam(required = false, defaultValue = SystemConstats.PAGE_DEFAULT_FROM) @PositiveOrZero Integer from,
             @RequestParam(required = false, defaultValue = SystemConstats.PAGE_DEFAULT_SIZE) @Positive Integer size) {
-        return eventService.getEventsByAdmin(users, states, categories, rangeStart, rangeEnd, from, size);
+            return eventService.getEventsByAdmin(users, states, categories, rangeStart, rangeEnd, from, size);
     }
 
     @PatchMapping("/{eventId}")
     @ResponseStatus(HttpStatus.OK)
     public ResultEventDto patchEventByAdmin(@PathVariable Long eventId,
-                                            @Valid @RequestBody UpdateEventDto updateEventAdminRequest) {
+                                          @Valid @RequestBody UpdateEventDto updateEventAdminRequest) {
         return eventService.editEventByAdmin(eventId, updateEventAdminRequest);
     }
 }
