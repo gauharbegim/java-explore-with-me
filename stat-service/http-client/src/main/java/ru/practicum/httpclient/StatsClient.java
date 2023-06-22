d6343e3f618e33408c4b9c14469b9b4b346270af
Gauhar <gauharbegimova@gmail.com>
2023-06-23 01:14:43 +0600

    fix: recall request add transaction

--- stat-service/http-client/src/main/java/ru/practicum/httpclient/StatsClient.java
+++ stat-service/http-client/src/main/java/ru/practicum/httpclient/StatsClient.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
-import ru.practicum.dto.HitDto;
+import ru.practicum.dto.EndpointHit;
 import ru.practicum.httpclient.constants.StatConstants;
 
 import java.time.LocalDateTime;
@@ -19,7 +19,7 @@ import java.util.Map;
 @Slf4j
 public class StatsClient extends BaseClient {
     @Autowired
-    public StatsClient(@Value("${stats-client.uri}") String serverUrl, RestTemplateBuilder builder) {
+    public StatsClient(@Value("${stats-server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(builder
                 .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
                 .requestFactory(HttpComponentsClientHttpRequestFactory::new)
@@ -28,28 +28,28 @@ public class StatsClient extends BaseClient {
     }
 
     public ResponseEntity<Object> saveHit(String appName, String uri, String ip, LocalDateTime timestamp) {
-        HitDto endpointHit = HitDto.builder()
+        EndpointHit endpointHit = EndpointHit.builder()
                 .app(appName)
                 .uri(uri)
                 .ip(ip)
-                .timeStamp(timestamp)
+                .timestamp(timestamp.format(StatConstants.DT_FORMATTER))
                 .build();
         return post(StatConstants.HIT_ENDPOINT, endpointHit);
     }
 
     public ResponseEntity<Object> getStat(LocalDateTime start, LocalDateTime end, List<String> uris) {
-        return getStat(start, end, uris, null);
+        return getStats(start, end, uris, null);
     }
 
     public ResponseEntity<Object> getStat(LocalDateTime start, LocalDateTime end) {
-        return getStat(start, end, null, null);
+        return getStats(start, end, null, null);
     }
 
     public ResponseEntity<Object> getStat(LocalDateTime start, LocalDateTime end, Boolean unique) {
-        return getStat(start, end, null, unique);
+        return getStats(start, end, null, unique);
     }
 
-    public ResponseEntity<Object> getStat(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
+    public ResponseEntity<Object> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
         if (start == null || end == null || start.isAfter(end)) {
             throw new IllegalArgumentException("Недопустимый временной промежуток.");
         }
--- stat-service/http-service/src/main/java/ru/practicum/httpservice/mapper/HitMapper.java
+++ stat-service/http-service/src/main/java/ru/practicum/httpservice/mapper/HitMapper.java
@@ -16,17 +16,6 @@ public class HitMapper {
         hit.setUri(dto.getUri());
         hit.setIp(dto.getIp());
         hit.setTimeStamp(LocalDateTime.parse(dto.getTimestamp(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-
         return hit;
-    }
-
-    public static HitOutputDto toOutputDto(HitEntity hit) {
-        return HitOutputDto.builder()
-                .id(hit.getId())
-                .app(hit.getApp())
-                .uri(hit.getUri())
-                .ip(hit.getIp())
-                .timeStamp(hit.getTimeStamp())
-                .build();
     }
 }
